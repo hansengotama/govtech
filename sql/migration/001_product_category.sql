9832575b38b073f68103bf70fbbedf869337c7e3
Hansen <hansen@192.168.1.11>
2023-10-18 04:14:58 +0700

    add category name into unique

--- sql/migration/001_product_category.sql
+++ sql/migration/001_product_category.sql
@@ -3,7 +3,7 @@
 CREATE TABLE product_categories (
     id serial PRIMARY KEY,
     guid uuid NOT NULL DEFAULT gen_random_uuid(),
-    name text NOT NULL,
+    name text UNIQUE NOT NULL,
 
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
